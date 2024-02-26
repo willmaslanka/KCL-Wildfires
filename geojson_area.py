5e26045adb727a71a96762e7c8e08ffd4288c503
willmaslanka <123586105+willmaslanka@users.noreply.github.com>
2024-02-26 15:38:07 +0000

    Creating Geojson_area.py
    Function to calculate the area of a geojson in Sq. Meters.

---
+++ geojson_area.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 15:26:43 2024
+
+@author: WMaslanka
+
+Need to install the "area" library (https://pypi.org/project/area/)
+"""
+
+from area import area
+import geojson
+
+fname = 'filepath-of-geojson'
+
+def plume_area_calc(fname: str) -> float:
+    """ Calculates the area of the geojson polygon, in Sq. Meters.
+    If wanted in Sq. km, then Sq. Meters / (10**6)"""
+    with open(fname) as f:
+        gj = geojson.load(f)
+    return area(gj['features'][0]['geometry'])
+
+val = plume_area_calc(fname)
