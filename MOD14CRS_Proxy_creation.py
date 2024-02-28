84c33bf56cd1172d96ed5e8e99ee8b47a7a78655
willmaslanka <123586105+willmaslanka@users.noreply.github.com>
2024-02-28 11:15:31 +0000

    Update MOD14CRS_Proxy_creation.py
    Added functionality to add "CoarseFireCounts" to the MOD14CRS -proxy creation script

--- MOD14CRS_Proxy_creation.py
+++ MOD14CRS_Proxy_creation.py
@@ -5,6 +5,7 @@ Created on Mon Feb 19 10:46:13 2024
 @author: k2262276
 """
 from pyhdf.SD import SD, SDC
+import numpy as np
 
 #%%
 def extract_hdf_2D(filename, var_name):
@@ -13,7 +14,7 @@ def extract_hdf_2D(filename, var_name):
     var = hdf.select(var_name)[:,:]
     return var
 
-def create_latlon_hdf(old_hdf, new_hdf):
+def create_latlon_hdf(old_latlon_file, fire_mask_file, new_hdf):
     """ 
     Writes the new hdf4 file
     
@@ -27,8 +28,15 @@ def create_latlon_hdf(old_hdf, new_hdf):
     filehdf  = SD(new_hdf, SDC.WRITE | SDC.CREATE)
     
     # Extracts the latitude and longitude from the MXD07 file
-    lat = extract_hdf_2D(old_hdf, 'Latitude')
-    lon = extract_hdf_2D(old_hdf, 'Longitude')
+    lat = extract_hdf_2D(old_latlon_file, 'Latitude')
+    lon = extract_hdf_2D(old_latlon_file, 'Longitude')
+    
+    # Extracts and sorts out the 
+    fm = extract_hdf_2D(fire_mask_file, 'fire mask') - 3 # Removes Not Processed Flags
+    fm[fm == 0] = 64 # Mimics MOD14CRS pixel count
+    fm[fm == 1] = 32 # Mimics MOD14CRS pixel count
+    fm[fm == 2] = 0 # Mimics MOD14CRS pixel count
+    cfm = coarsen_array(fm, 5) # Takes the average pixel count of each 5x5 window
     
     # Saves attribute "title" to be the filename of the MXD07 file used
     att = filehdf.attr('title')
@@ -37,21 +45,47 @@ def create_latlon_hdf(old_hdf, new_hdf):
     # Create the latitude dataset : set its name, data type and dimensions
     sds_name = "Latitude"
     sds = filehdf.create(sds_name, SDC.FLOAT32, lat.shape)
-    sds.dim(0).setname("Cell_Along_Swath:mod07")
-    sds.dim(1).setname("Cell_Across_Swath:mod07")
+    sds.dim(0).setname("number_of_coarse_resolution_scan_lines")
+    sds.dim(1).setname("coarse_resolution_pixels_per_scan_line")
     sds[:] = lat    # Write the latitude to it
     sds.endaccess() # Terminate access to the data set
     
     # Create the longitude dataset : set its name, data type and dimensions
     sds_name = "Longitude"
     sds = filehdf.create(sds_name, SDC.FLOAT32, lon.shape)
-    sds.dim(0).setname("Cell_Along_Swath:mod07")
-    sds.dim(1).setname("Cell_Across_Swath:mod07")
+    sds.dim(0).setname("number_of_coarse_resolution_scan_lines")
+    sds.dim(1).setname("coarse_resolution_pixels_per_scan_line")
+    sds[:] = lon    # Write the longitude to it
+    sds.endaccess() # Terminate access to the data set
+    
+    # Create the longitude dataset : set its name, data type and dimensions
+    sds_name = "CoarseFireCounts"
+    sds = filehdf.create(sds_name, SDC.UINT8, cfm.shape)
+    sds.dim(0).setname("number_of_coarse_resolution_scan_lines")
+    sds.dim(1).setname("coarse_resolution_pixels_per_scan_line")
     sds[:] = lon    # Write the longitude to it
     sds.endaccess() # Terminate access to the data set
     
     # Close the file
     filehdf.end()
+    
+def coarsen_array(array, coarseness=5):
+    """ 
+    Coarsens the array by a factor of coarseness.
+    """
+    shape = np.array(array.shape, dtype=int)
+    new_shape = coarseness * np.ceil(shape / coarseness).astype(int)
+
+    # Create the zero-padded array and assign it with the old density
+    zp_pop_density = np.zeros(new_shape)
+    zp_pop_density[:shape[0], :shape[1]] = array
+
+    # Now use the same method as before
+    temp = zp_pop_density.reshape((new_shape[0] // coarseness, coarseness,
+                                   new_shape[1] // coarseness, coarseness))
+    coarse_array = np.round(np.mean(temp, axis=(1,3)) / 32) * 32 # Rounds to nearest 32
+    
+    return coarse_array
 
 #%% User Variables
 old_fname = 'filepath-to-MOD06'
